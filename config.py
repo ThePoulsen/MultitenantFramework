7330856ccf4bc5bb2e6db50da18978560a4258b1
ThePoulsen <henrik@vipilon.dk>
2017-02-14 11:57:05 +0100

    commit

--- config.py
+++ config.py
@@ -4,33 +4,23 @@
 import os
 import vars
 
-# System environments set for security purposes
-mail                                = os.environ['mail']
-mailPass                            = os.environ['mailPass'].replace("'","")
-secretKey                           = os.environ['secretKey']
-db                                  = os.environ['db']
-mailSender                          = os.environ['mailSender']
-mailServer                          = os.environ['mailServer']
-mailPort                            = os.environ['mailPort']
-mailSSL                             = os.environ['mailSSL']
-
 # Flask settings
-SECRET_KEY                          = secretKey
-SQLALCHEMY_DATABASE_URI             = db
+SECRET_KEY                          = os.environ['secretKey']
+SQLALCHEMY_DATABASE_URI             = os.environ['db']
 SQLALCHEMY_TRACK_MODIFICATIONS      = False
 JSON_AS_ASCII                       = False #unicode settings
 TEMPLATES_AUTO_RELOAD               = True
 
 # Flask mail settings
-MAIL_USERNAME                       = mail
-MAIL_PASSWORD                       = mailPass
-MAIL_DEFAULT_SENDER                 = mailSender
-MAIL_SERVER                         = mailServer
-MAIL_PORT                           = int(mailPort)
-MAIL_USE_SSL                        = bool(mailSSL)
+MAIL_USERNAME                       = os.environ['MAIL_USERNAME']
+MAIL_PASSWORD                       = os.environ['mailPass']
+MAIL_DEFAULT_SENDER                 = os.environ['mailSender']
+MAIL_SERVER                         = os.environ['mailServer']
+MAIL_PORT                           = int(os.environ['mailPort'])
+MAIL_USE_SSL                        = bool(os.environ['mailSSL'])
 
 # Bcrypt settings
-BCRYPT_LOG_ROUNDS                   = 12
+BCRYPT_LOG_ROUNDS                   = int(os.environ['BCRYPT_LOG_ROUNDS'])
 
 # Flask-htmlmin settings
 MINIFY_PAGE                         = True
